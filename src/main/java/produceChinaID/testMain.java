dac1277b38e6f5d35ab19d97eed327e3cc942616
riitei <riitei@x260>
2017-08-23 14:46:41 +0800

    城市 生日 序號   組成一組數列 再算驗證碼　（去除多餘程式碼）

--- src/main/java/produceChinaID/testMain.java
+++ src/main/java/produceChinaID/testMain.java
@@ -21,27 +21,6 @@ public class testMain {
         for(ComposeID composeID : compose.composeIDs() ){
             System.out.println(composeID.getComposeID());
         }
-//        City city = new City(500101);
-//        Birthday birthday = new Birthday();
-//        birthday.setStratBirthday(1987, 5, 8);
-//        birthday.setEndBirthday(1987, 5, 9);
-//        birthday.birthday();
-//        Sequence sequence = new Sequence();
-//        ProduceChinaID produceChinaID = new ProduceChinaID();
-//        sequence.sequence();
-//        Compose compose = new Compose();
-//        compose.compose();
-        //
-//        int i = 0;
-//        for (BirthdayID birthdayList : birthday.getBirthday()) {
-//            for (SequenceID sequenceList : sequence.getSequence()) {
-//                produceChinaID.produceChinaID(city.getCity(), birthdayList.getBirthday(), sequenceList.getSequence());
-//                for (ChinaID cid : produceChinaID.getChinaIDs()) {
-//                    System.out.println(i++ + " ===> " + cid.getChinaID());
-//                }
-//            }
-//            //    produceChinaID.test(); error
-//        }
 
     }
 }
